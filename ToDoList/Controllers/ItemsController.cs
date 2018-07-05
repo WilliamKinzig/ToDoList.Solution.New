5e428dcc874696de337b92ec7ce446a05fac3e9d
Guest User <Guest@epicodus-31.local>
2018-07-05 10:16:28 -0700

    add clear all Finished Using HTTP Post

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,22 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
+using System.Collections.Generic;
 
 namespace ToDoList.Controllers
 {
-  public class ItemsController : Controller
-  {
-
-    [HttpGet("/items")]
-    public ActionResult Index()
+    public class ItemsController : Controller
     {
-      Item newItem = new Item(Request.Query["new-item"]);
-      return View(newItem);
-    }
 
-    [HttpGet("/items/new")]
-    public ActionResult CreateForm()
-    {
-      return View();
+        [HttpGet("/items")]
+        public ActionResult Index()
+        {
+            //Item newItem = new Item(Request.Query["new-item"]);
+            //return View(newItem);
+            List<Item> allItems = Item.GetAll();
+            return View(allItems);
+        }
+
+        [HttpGet("/items/new")]
+        public ActionResult CreateForm()
+        {
+            return View();
+        }
+
+        [HttpPost("/items")]
+        public ActionResult Create()
+        {
+            Item newItem = new Item (Request.Form["new-item"]);
+            newItem.Save();
+            List<Item>allItems = Item.GetAll();
+            return View("Index", allItems);
+        }
+
+        [HttpPost("/items/delete")]
+        public ActionResult DeleteAll()
+        {
+            Item.ClearAll();
+            return View();
+        }
+
     }
-  }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -3,39 +3,45 @@ using System.Collections.Generic;
 
 namespace ToDoList.Models
 {
-  public class Item
-  {
-    private string _description;
-    private static List<Item> _instances = new List<Item> {};
+    public class Item
+    {
+        private string _description;
+        private static List<Item> _instances = new List<Item> {};
 
-    public Item(string description)
-    {
-      _description = description;
+        public Item(string description)
+        {
+            _description = description;
+        }
+
+        //Check if Unit Test setup correctly
+        // public bool Default()
+        // {
+        //     return true;
+        // }
+
+        public string GetDescription()
+        {
+            return _description;
+        }
+
+        public void SetDescription(string newDescription)
+        {
+            _description = newDescription;
+        }
+
+        public static List<Item> GetAll()
+        {
+            return _instances;
+        }
+
+        public void Save()
+        {
+            _instances.Add(this);
+        }
+
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
     }
-    //Check if Unit Test setup correctly
-    public bool Default()
-    {
-      return true;
-    }
-    public string GetDescription()
-    {
-      return _description;
-    }
-    public void SetDescription(string newDescription)
-    {
-      _description = newDescription;
-    }
-    public static List<Item> GetAll()
-    {
-      return _instances;
-    }
-    public void Save()
-    {
-      _instances.Add(this);
-    }
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
-  }
 }
