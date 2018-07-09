7804d0bc861cb3c7471d05afb2a58a8e66ddabef
WilliamKinzig <wippawill2@gmail.com>
2018-07-08 21:33:22 -0700

    -failed Creating the Database Object _ rewrite our GetAll()

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,56 +1,81 @@
-using System;
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using ToDoList;
+using System;
 
 namespace ToDoList.Models
 {
-    public class Item
+  public class Item
+  {
+    private int _id;
+    private string _description;
+
+    public Item(string Description, int Id = 0)
     {
-        private string _description;
-        private int _id;
-        private static List<Item> _instances = new List<Item> {};
+      _id = Id;
+      _description = Description;
+    }
 
-        public Item(string description)
-        {
-            _description = description;
-            _instances.Add(this);
-            _id = _instances.Count;
-        }
-
-        public string GetDescription()
-        {
-            return _description;
-        }
-
-        public void SetDescription(string newDescription)
-        {
-            _description = newDescription;
-        }
+    //...GETTERS AND SETTERS WILL GO HERE...
 
         public static List<Item> GetAll()
         {
-            return _instances;
+            List<Item> allItems = new List<Item> {};
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM items;";
+            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while(rdr.Read())
+            {
+              int itemId = rdr.GetInt32(0);
+              string itemDescription = rdr.GetString(1);
+              Item newItem = new Item(itemDescription, itemId);
+              allItems.Add(newItem);
+            }
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return allItems;
         }
-
-        public int GetId()
-        {
-            return _id;
-        }
-
-        public void Save()
-        {
-            _instances.Add(this);
-        }
-
-        public static void ClearAll()
-        {
-            _instances.Clear();
-        }
-
-        public static Item Find(int searchId)
-        {
-            return _instances[searchId-1];
-        }
-
-
+        //private static List<Item> _instances = new List<Item> {};
+        //
+        // public Item(string description)
+        // {
+        //     _description = description;
+        //     _instances.Add(this);
+        //     _id = _instances.Count;
+        // }
+        //
+        // public string GetDescription()
+        // {
+        //     return _description;
+        // }
+        //
+        // public void SetDescription(string newDescription)
+        // {
+        //     _description = newDescription;
+        // }
+        // public int GetId()
+        // {
+        //     return _id;
+        // }
+        //
+        // public void Save()
+        // {
+        //     _instances.Add(this);
+        // }
+        //
+        // public static void ClearAll()
+        // {
+        //     _instances.Clear();
+        // }
+        //
+        // public static Item Find(int searchId)
+        // {
+        //     return _instances[searchId-1];
+        // }
     }
 }
